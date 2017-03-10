b0f6eafaa3a5afafdd1640698c63acf02b9df3dc
Beverly Lanning <beverly_lanning@reyrey.com>
2017-03-10 13:16:11 -0500

    added 'at a glance' structure for today's tasks

---
+++ includes/ataglance.php
@@ -0,0 +1,85 @@
+<div class="col2-lg box-soft-round">
+	<p class="light">At a glance</p>
+	<h3>Today's Tasks</h3>	
+
+	<div>
+		<div class="box-soft mb-075">
+			<div class="col2-lg border-rt-gray">
+				<div class="col2 bold pl-0 pt-0">Lorem ipsum dolor</div>
+				<p class="col2 rt light pl-0 pt-0">Project 1</p>
+				<p>Sea te ceteros facilisi, nam in tempor quaeque habemus... <a class="more">Read more</a></p>
+			</div>
+			<form class="col2-sm">
+			<div>
+				<select>
+					<option value="Not started">Not started</option>
+					<option value="Red folder">Red folder</option>
+					<option value="Pending">Pending</option>
+					<option value="Complete">Complete</option>
+				</select>
+			</div>
+		</form>
+		</div>
+	</div>
+
+	<div>
+		<div class="box-soft mb-075">
+			<div class="col2-lg border-rt-gray">
+				<div class="col2 bold pl-0 pt-0">Eu solet officiis eam</div>
+				<p class="col2 rt light pl-0 pt-0">Project 3</p>
+				<p>Vis nobis democritum ut, ne persius alterum necessitatibus qu... <a class="more">Read more</a></p>
+			</div>
+			<form class="col2-sm">
+			<div>
+				<select>
+					<option value="Not started">Not started</option>
+					<option value="Red folder">Red folder</option>
+					<option value="Pending">Pending</option>
+					<option value="Complete">Complete</option>
+				</select>
+			</div>
+		</form>
+		</div>
+	</div>
+
+	<div>
+		<div class="box-soft mb-075">
+			<div class="col2-lg border-rt-gray">
+				<div class="col2 bold pl-0 pt-0">His ad ludus mucius</div>
+				<p class="col2 rt light pl-0 pt-0">Project 1</p>
+				<p>Est debet utroque id. Ei vel adipisci lobortis erroribus... <a class="more">Read more</a></p>
+			</div>
+			<form class="col2-sm">
+			<div>
+				<select>
+					<option value="Not started">Not started</option>
+					<option value="Red folder">Red folder</option>
+					<option value="Pending">Pending</option>
+					<option value="Complete">Complete</option>
+				</select>
+			</div>
+		</form>
+		</div>
+	</div>
+
+	<div>
+		<div class="box-soft mb-075">
+			<div class="col2-lg border-rt-gray">
+				<div class="col2 bold pl-0 pt-0">At ius essent lobortis, an per integre</div>
+				<p class="col2 rt light pl-0 pt-0">Project 1</p>
+				<p>rompta epicuri quaerendum nec in, ne tempor tacimates pericula... <a class="more">Read more</a></p>
+			</div>
+			<form class="col2-sm">
+			<div>
+				<select>
+					<option value="Not started">Not started</option>
+					<option value="Red folder">Red folder</option>
+					<option value="Pending">Pending</option>
+					<option value="Complete">Complete</option>
+				</select>
+			</div>
+		</form>
+		</div>
+	</div>
+
+</div>
